d35280ebd6840b3fe89d4d36afdf23fe92e4caf4
Lalt_Saini <lalitpsaini@gmail.com>
2019-12-11 23:11:09 +0530

    page change and indentation problem solved

--- pdf.py
+++ pdf.py
@@ -1,6 +1,9 @@
 from reportlab.pdfgen import canvas
 import json
 import functions as f
+import reportlab.rl_config
+reportlab.rl_config.warnOnMissingFontGlyphs = 0
+from reportlab.pdfbase import pdfmetrics
 
 with open('data.json') as data_file:
     data = json.load(data_file)
@@ -11,34 +14,55 @@ param=f.makeParam(data)
 
 horz_k=100
 horz_val=0
-indent=50
-lineSpacing=20
+indent=10
+lineSpacing=30
 
 def writeTextTo(c):
     ver=700
     for item in param:
         if str(type(data[item])).find('dict')!=-1:
+            c.setFont('Times-Bold', 16)
             c.drawString(horz_k, ver, item.capitalize()+":")
             ver=ver-lineSpacing
-            horz_d=horz_k+len(item)+indent
+            if ver < 50:
+                c.showPage()
+                ver=750
+
+            horz_d=horz_k+c.stringWidth(item, 'Times-Roman', 16)+indent
             for subItems in data[item]:
+                c.setFont('Times-Bold', 16)
                 c.drawString(horz_d, ver, subItems.capitalize()+":")
-                horz_val=horz_d+len(subItems)+indent
+                c.setFont('Courier', 16)
+                horz_val=horz_d+c.stringWidth(subItems, 'Times-Roman', 16)+indent
                 c.drawString(horz_val, ver, str(data[item][subItems]))
                 ver=ver-lineSpacing
+                if ver < 50:
+                    c.showPage()
+                    ver=750
+
 
         elif str(type(data[item])).find('list')!=-1:
+            c.setFont('Times-Bold', 16)
             c.drawString(horz_k, ver, item.capitalize()+":")
-            horz_val=horz_k+len(item)+indent
-            for subItems in data[item]:                
+            horz_val=horz_k+c.stringWidth(item, 'Times-Roman', 16)+indent
+            for subItems in data[item]:
+                c.setFont('Courier', 16)                
                 c.drawString(horz_val, ver, str(subItems))
                 ver=ver-lineSpacing
+                if ver < 50:
+                    c.showPage()
+                    ver=750
 
         else:
+            c.setFont('Times-Bold', 16)
             c.drawString(horz_k, ver, item.capitalize()+":")
-            horz_val=horz_k+len(item)+indent
+            horz_val=horz_k+c.stringWidth(item, 'Times-Roman', 16)+indent
+            c.setFont('Courier', 16)
             c.drawString(horz_val, ver, str(data[item]))
             ver=ver-lineSpacing
+            if ver < 50:
+                c.showPage()
+                ver=750
         
 
 c=canvas.Canvas("hello.pdf")
